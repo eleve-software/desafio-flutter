c63ea3b47ce9575ba69f27e6382d5a269cef402e
robsonsilv4 <robsonsilv410@gmail.com>
2020-09-27 17:56:00 -0300

    fix: add detail route

--- pull_request_manager/lib/routes.dart
+++ pull_request_manager/lib/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 
 import 'ui/screens/home_screen.dart';
+import 'ui/screens/pull_request_detail_screen.dart';
 import 'ui/screens/pull_request_screen.dart';
 import 'ui/screens/repository_screen.dart';
 
@@ -10,10 +11,12 @@ class Routes {
   static const String home = '/home';
   static const String repository = '/repository';
   static const String pullRequest = '/pullrequest';
+  static const String pullRequestDetail = '/pullrequest-detail';
 
   static final routes = <String, WidgetBuilder>{
     home: (context) => HomeScreen(),
     repository: (context) => RepositoryScreen(),
     pullRequest: (context) => PullRequestScreen(),
+    pullRequestDetail: (context) => PullRequestDetailScreen(),
   };
 }
